c57cb78f6cc6c9bd483ed9b4fbeda6446d0b868f
yossyoss <yosi0679@gmail.com>
2019-02-11 22:10:04 +0200

    adding functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 
-
 class App extends Component {
+  state = { resource: "posts" };
   render() {
-    return <div className="App">app</div>;
+    return (
+      <div>
+        <div>
+          <button onClick={() => this.setState({ resource: "posts" })}>
+            Posts
+          </button>
+          <button onClick={() => this.setState({ resource: "todos" })}>
+            Todos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+    );
   }
 }
 
